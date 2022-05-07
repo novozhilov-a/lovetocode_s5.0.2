1e1420d7a6a21b979d7430b0852d43acd8558a69
Aleksey Novozhilov <novojilov@inbox.ru>
2022-05-07 20:53:28 +0200

    removed logging set up

--- src/com/lovetocode/MyAppSpring.java
+++ src/com/lovetocode/MyAppSpring.java
@@ -1,35 +1,13 @@
 package com.lovetocode;
 
-import com.lovetocode.config.Logging;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-
 public class MyAppSpring {
-    /*static {
-        String configPath = MyAppSpring.class
-                .getClassLoader()
-                .getResource("logging.properties")
-                .getFile();
-        configPath = configPath.replace("%20", " ");
-        System.setProperty("java.util.logging.config.file", configPath);
-        //init JUL with logging.properties
-        try {
-            LogManager.getLogManager().readConfiguration();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
-
     private static final Log log = LogFactory.getLog(MyAppSpring.class);
 
     public static void main(String[] args) throws InterruptedException {
-        Logging.setLevel(Level.ALL);
-
         log.trace("trace");
         log.debug("debug");
         log.info("info");
@@ -46,7 +24,7 @@ public class MyAppSpring {
                 "myCoach.getDailyWorkout() = " + myCoach.getDailyWorkout());
         System.out.println(
                 "myCoach.getDailyFortune() = " + myCoach.getDailyFortune());
-        Thread.sleep(100l);
+        Thread.sleep(100L);
         //close context
         context.close();
     }
